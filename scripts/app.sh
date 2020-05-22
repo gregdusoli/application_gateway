b1c91061e8c9f876cdc83860a9c47325b6753582
Guilherme Gomes <gtechmedia@gmail.com>
2020-05-22 19:56:46 -0300

    Finish automation scripts

--- scripts/app.sh
+++ scripts/app.sh
@@ -2,8 +2,23 @@
 
 clear
 
-# Define o Path de instalação do Application Gateway
-APPGTW_PATH=$(echo -e ~/application_gateway)
+# Define os paths de instalação do Application Gateway e projetos
+pwdir=$(dirname "$0")
+if [[ "$pwdir" =~ ["./"] ]]; then
+  if [[ "$pwdir" =~ ["scripts"] ]]; then
+    pwdir=$(pwd)
+  else 
+    cd ..
+    pwdir=$(pwd)
+  fi
+fi
+APPGTW_PATH=$pwdir
+PUBLIC_HTML="public_html"
+
+# Define outras variáveis necessárias
+APPSVC_TYPE=
+APPSVC_NAME=
+APPSVC_PATH=
 
 # Formatação de texto
 bold="\033[1m"
@@ -13,50 +28,76 @@ red="\033[31m"
 clear="\033[0m"
 
 function option1 {
+  # Invoce a function que efine se o tipo do container é 'application' ou 'service'
+  setAppSvcType
+
+  # Move o ponteiro para o diretório correspondente ao tipo (application/service)
+  cd $APPSVC_TYPE
+  
   # Solicita o nome da aplicação que será também o nome do subdiretório (public_html/APP_NAME/)
-  echo -e "$bold\n- Informe o nome da nova aplicação (nome semântico em Snake Case - ex: 'api_gateway')$clear"
-  read appname
+  echo -e "$bold\n- Qual é o nome do container da aplicação (exatamente o mesmo que está no Docker Compose do container)?$clear"
+  read APPSVC_NAME
 
   # Solicita o link do repositório de código
-  echo -e "$bold\n- Informe o link do repositório da aplicação$clear"
+  echo -e "$bold\n- Qual é o link do repositório da aplicação?$clear"
   read repolink
 
   # Clona o respositório da aplicação
-  $(git clone $repolink $appname )
+  $(git clone $repolink $APPSVC_NAME )
+
+  # Testar o resultado do comando git clone, caso haja erro encerra a execução
+  if [[ $? == 128 ]]; then
+    echo -e "$bold$red\nO repositório informado não existe ou é privado... execução abortada!$clear"
+    exit 1
+  fi
 
   # Acessa o diretório da aplicação clonada
-  cd $appname
+  cd $APPSVC_NAME
 
   # Verifica se o arquivo .env.example existe e, caso sim, renomeia para .env
   if [[ -e .env.example ]]; then
     createEnvFile
   fi
 
-  # Executa a função que sube os serviços do container
-  dockerComposeUp
+  # Define o caminho absoluto da aplicação
+  APPSVC_PATH="$APPGTW_PATH/$PUBLIC_HTML/$APPSVC_TYPE/$APPSVC_NAME"
+
+  # Invoca a execução da function responsável pelo processo de Dockerização
+  dockerCompose
 }
 
 function option2 {
-  # Solicita o local do arquivo docker-compose.yml
-  echo -e "$bold\n- Informe o local do arquivo docker-compose.yml (relativo ao diretório public_html):$clear"
-  read dockerfile
+  # Informa o usuário sobre o local padrão para applications e services
+  echo -e "$bold$yellow\nIMPORTANTE: Este script pressupõem que sua aplicação esteja dentro do diretório $APPGTW_PATH/public_html/.$clear"
 
-  # Confirma o local do arquivo docker-compose.yml
-  if [[ -e "$APPGTW_PATH/public_html/${dockerfile}/docker-compose.yml" ]]; then
-    echo -e "$green\nO Docker Composer foi validado em: ($APPGTW_PATH/public_html/${dockerfile}/docker-compose.yml)!\n$clear"
+  # Invoce a function que efine se o tipo do container é 'application' ou 'service'
+  setAppSvcType
 
-    # Acessa o diretório da aplicação clonada
-    cd "$APPGTW_PATH/public_html/$dockerfile"
-
-    # Executa a função que sube os serviços do container
-    dockerComposeUp
+  # Move o ponteiro para o diretório correspondente ao tipo (application/service)
+  cd $APPSVC_TYPE
+  
+  # Solicita o nome da aplicação que será também o nome do subdiretório (public_html/APP_NAME/)
+  echo -e "$bold\n- Qual é o nome do container da aplicação (nome do container Docker, omitido o prefixo de tipo)?$clear"
+  read APPSVC_NAME
+  
+  # Acessa o diretório da aplicação clonada
+  if [[ -e $APPSVC_NAME ]]; then
+    cd $APPSVC_NAME
   else
-    echo -e "$bold$red\nO arquivo docker-compose.yml informado é inválido ou não existe em $APPGTW_PATH/public_html/${dockerfile}\n$clear"
-    exit 0
+    echo -e "$bold$red\nO caminho informado não foi encontrado. Execução abortada!$clear"
+    exit 1
   fi
 
-  # Executa a função que sube os serviços do container
-  dockerComposeUp
+  # Verifica se o arquivo .env.example existe e, caso sim, renomeia para .env
+  if [[ -e .env.example ]]; then
+    createEnvFile
+  fi
+
+  # Define o caminho absoluto da aplicação
+  APPSVC_PATH="$APPGTW_PATH/$PUBLIC_HTML/$APPSVC_TYPE/$APPSVC_NAME"
+
+  # Invoca a execução da function responsável pelo processo de Dockerização
+  dockerCompose
 }
 
 function option3 {
@@ -67,9 +108,9 @@ function option3 {
   ./proxy.sh
 }
 
-function dockerComposeUp {
+function dockerCompose {
   # Verifica a existência de um arquivo 'docker-compose.yml antes de subir o container para evitar erro'
-  if [[ -e ./docker-compose.yml || -e ./docker-compose.yaml ]]; then
+  if [[ -e "$APPSVC_PATH/docker-compose.yml" || -e "$APPSVC_PATH/docker-compose.yaml" ]]; then
     # Executa 'docker-compose up -d' e sobe o container
     docker-compose up -d
 
@@ -80,22 +121,24 @@ function dockerComposeUp {
       # Executa o script para criação das regras de redirecionamento
       echo -e "$yellow\nPor fim, vamos executar o script de criação das regras de redirecionamento para a nova aplicação...\n$clear"
       
-      # Move o cursor para o diretório de scripts
-      cd $APPGTW_PATH/scripts/
-      ./proxy.sh 1 $appname
+      # Move o cursor para o diretório raiz do Application Gateway
+      cd $APPGTW_PATH
+
+      # Invoca o script de criação das regras de Streams e Proxy
+      ./scripts/proxy.sh 1 "$APPSVC_TYPE-$APPSVC_NAME"
       
       # Verifica se a saída do comando anterior gerou algum erro
       if [[ $? -eq 0 ]]; then
         echo -e "$green\nRegras de redirecionamento criadas com sucesso!\n$clear"
       else 
-        echo -e "$red\nErro ao aplicar as novas configurações...restart o container do Application Gateway manualmente.\n$clear"
+        echo -e "$red\nErro ao aplicar as novas configurações...verifique possíveis inconsistências em seu application/service.\n$clear"
       fi
     else
-      echo -e "$red\nNão foi possível subir os serviços do container! Execute este passo manualmente...\n$clear"
+      echo -e "$red\nNão foi possível subir os serviços do container... execute este passo manualmente.\n$clear"
     fi
   else 
-    echo -e "$yellow\nO arquivo 'docker-compose.y(a)ml' não foi localizado... suba o container manualmente e, a seguir, execute 'npm run proxy' para criar as regras de redirecionamento.\n$clear"
-    echo -e "$bold$green\nAplicação criada com sucesso!\n$clear"
+    echo -e "$yellow\nArquivo docker-compose.yml não localizado! Revise seu application/service e execute manualmente os passos restantes.\n$clear"
+    echo -e "$bold\nApplication/Service criado com sucesso, porém a execução gerou alerta(s)!\n$clear"
     exit 0;
   fi
 }
@@ -108,6 +151,25 @@ function createEnvFile {
 
   echo -e "$yellow$bold$clear$yellow\nForneça os dados requeridos pelo arquivo Env, em seguida, execute este script novamente selecionando a opção 2.\n$clear"
   exit 0
+}
+
+function setAppSvcType {
+  echo -e "$bold\n- O container que você vai implementar é de qual tipo?$clear"
+  echo -e "(Em caso de dúvida, considere de maneira generalista 'application' como software com UI e 'service' como software sem UI)"
+  echo -e " - Application \t(1)"
+  echo -e " - Service \t(2)"
+
+  optiontype=0
+  while [ $optiontype == 0 ] || [ $optiontype -gt 2 ]; do
+    echo -e "$bold Digite a opção correspondente:$clear"
+    read optiontype
+  done
+
+  case $optiontype in
+    1) APPSVC_TYPE='application';;
+    2) APPSVC_TYPE='service';;
+    *) APPSVC_TYPE='Tipo de inválido';;
+  esac
 }
 
 ##################################
@@ -131,7 +193,7 @@ echo -e "(?) SAIR"
 read option
 
 # Acessa o diretório padrão de projetos para criação da aplicação
-cd ~/public_html/
+cd "$APPGTW_PATH/$PUBLIC_HTML"
 
 if [[ $option == 1 ]]; then
 
--- scripts/proxy.sh
+++ scripts/proxy.sh
@@ -2,8 +2,27 @@
 
 clear
 
-# Define o Path de instalação do Application Gateway
-APPGTW_PATH=$(echo -e ~/application_gateway)
+# Define os paths de instalação do Application Gateway e projetos
+pwdir=$(dirname "$0")
+if [[ "$pwdir" =~ ["./"] ]]; then
+  if [[ "$pwdir" =~ ["scripts"] ]]; then
+    pwdir=$(pwd)
+  else 
+    cd ..
+    pwdir=$(pwd)
+  fi
+fi
+APPGTW_PATH=$pwdir
+APPSVC_PATH="public_html"
+
+# Define outras variáveis necessária
+if [[ $# -gt 1 ]]; then
+  APPSVC_NAME=$2
+else
+  APPSVC_NAME=
+fi
+SUBDOMAIN=
+PORT=
 
 # Formatação de texto
 bold="\033[1m"
@@ -12,36 +31,29 @@ yellow="\033[33m"
 red="\033[31m"
 clear="\033[0m"
 
-# Associa variáveis locais aos parâmetros recebidos do caller
-if [[  $# -eq 3  ]]; then
-  option=$1
-  subdomain=$2
-  appname=$3
-fi
-
 function option1 {
   # Solicita o nome do domínio principal do servidor (CN = Canonical Name)
   echo -e "$bold\nPra começar, informe o Canonical Name que deseja usar (Nome raiz de domínio):$clear"
   read domain
 
-  if [[ -z "$subdomain" ]]; then
-    echo -e "$bold\n- Qual subdomínio deve ser vinculado? (FQDN = [subdomain].$domain)$clear"
-    read subdomain
-  fi
+  # Solicita o nome do subdomínio associado a application/service do usuário
+  echo -e "$bold\n- Qual subdomínio você quer vincular ao container ([subdomain].$domain)?$clear"
+  read SUBDOMAIN
 
-  if [[ -z "$appname" ]]; then
-    echo -e "$bold\n- Qual o nome do container da aplicação? (Você deve informar exatamente o mesmo que consta no Docker Compose do container)$clear"
-    read appname
+  # Verifica se a informação do path do application/service foi recebido como parâmetro do script app.sh
+  if [[ -z $APPSVC_NAME ]]; then
+    echo -e "$bold\n- Qual é o nome do container da aplicação (exatamente o mesmo que está no Docker Compose do container)?$clear"
+    read APPSVC_NAME
   fi
 
   echo -e "$bold\n- Em qual porta sua aplicação está escutando (para redirecionamento de requisições)?$clear"
-  read port
+  read PORT
 
   echo -e "$green ________________________________________________" # 48 characters
   echo -e "\t"
   echo -e "$bold     A seguinte regra de Proxy será criada: $clear$green"
-  echo -e "        FQDN: $subdomain.$domain"
-  echo -e "        Container: $appname:$port"
+  echo -e "        FQDN: $SUBDOMAIN.$domain "
+  echo -e "        Container: $APPSVC_NAME:$PORT "
   echo -e "________________________________________________$clear"
 
   echo -e "$bold$yellow\n*NOTA:$clear \033[33mCaso ainda não tenha criado o container da aplicação, não prossiga com este wizard e execute o script 'app.sh'.$clear"
@@ -50,59 +62,87 @@ function option1 {
   read confirm
 
   if [[ "$confirm" =~ [sS] ]]; then
-    # Acessa o diretório para criação do arquivo com a regra de Proxy
-    cd conf.d
-
-    # Cria o arquivo da regra de Proxy
-    proxy_file="$subdomain.conf"
-    touch $(echo "$proxy_file")
-
-    proxy_rule="server {
-      listen 80;
-      listen [::]:80;
-
-      server_name $subdomain.eqi.life;
-
-      location / {
-        proxy_pass http://$subdomain;
-      }
-    }"
-
-    # Grava o conteúdo da regra de Proxy no arquivo
-    echo "$proxy_rule" >> $proxy_file
-
-    # Acessa o diretório para criação do arquivo com a regra de Proxy
-    cd ../upstreams
-
-    # Cria o arquivo da regra de Upstream
-    upstream_file="$subdomain.conf"
-    touch $(echo "$upstream_file")
-
-    upstream_rule="upstream $subdomain {
-      server $appname:$port;
-    }"
-
-    # Grava o conteúdo da regra de Proxy no arquivo
-    echo "$upstream_rule" >> $upstream_file
-
+    # Invoca as functions que criar os arquivos de Stream e Proxy
+    proxyFileCreate
+    upstreamFileCreate
+    
     # Valida as regras criadas e somente prossegue em caso de sucesso
     cd $APPGTW_PATH
     echo $(docker-compose exec service-application_gateway nginx -t)
     
-    if [[ $? -eq 1 ]]; then
+    # Testa as regras criadas e se tudo estiver ok, reinicia o serviço do Nginx
+    if [[ $? -eq 0 ]]; then
+      echo $(docker-compose exec service-application_gateway nginx -s reload)
+
+      if [[ $? -eq 0 ]]; then
+        echo -e "$bold$green\nRegras de Stream e Proxy criadas com sucesso!\n$clear"
+      fi
+    else
       echo -e "$red$bold\nOcorreu um erro ao criar as regras de Stream e Proxy!\nO script foi abortado...$clear"
       exit 1
-    fi
-
-    # Reinicia o serviço do Nginx e aplica as regras criadas
-    echo $(docker-compose exec service-application_gateway nginx -s reload)
-
-    if [[ $? -eq 0 ]]; then
-      echo -e "$bold$green\nRegras de Stream e Proxy criadas com sucesso!\n$clear"
     fi
   else
     echo -e "$bold$yellow\nCriação da regra cancelada!\n$clear"
     exit 0
+  fi
+}
+
+function proxyFileCreate {
+  # Monta a regra de Proxy antes da criação do arquivo
+  proxy_rule="server {
+    listen 80;
+    listen [::]:80;
+
+    server_name $SUBDOMAIN.eqi.life;
+
+    location / {
+      proxy_pass http://$SUBDOMAIN;
+    }
+  }"
+
+  # Antes de criar o arquivo, verifica se já existe um com mesmo nome e deleta
+  proxy_file="$APPGTW_PATH/nginx/conf.d/$SUBDOMAIN.conf"
+  
+  if [[ -e "$proxy_file" ]]; then
+    rm "$proxy_file"
+  fi
+
+  touch "$proxy_file"
+
+  # Grava o conteúdo da regra de Proxy no arquivo
+  echo "$proxy_rule" >> $proxy_file
+
+  # Retorna o status da criação do arquivo
+  if [[ -e $proxy_file ]]; then
+    echo -e "$bold$green\nO arquivo $proxy_file foi criado com sucesso!$clear"
+  else
+    echo -e "$bold$red\nErro ao criar arquivo $proxy_file...$clear"
+  fi
+}
+
+function upstreamFileCreate {
+  # Monta a regra de Upstream antes da criação do arquivo
+  upstream_rule="upstream $SUBDOMAIN {
+    server $APPSVC_NAME:$PORT;
+  }"
+
+  # Antes de criar o arquivo, verifica se já existe um com mesmo nome e deleta
+  upstream_file="$APPGTW_PATH/nginx/upstreams/$SUBDOMAIN.conf"
+  
+  if [[ -e "$upstream_file" ]]; then
+    rm "$upstream_file"
+  fi
+
+  touch "$upstream_file"
+
+  # Grava o conteúdo da regra de Proxy no arquivo
+  echo "$upstream_rule" >> $upstream_file
+
+  # Retorna o status da criação do arquivo
+  if [[ -e $upstream_file ]]; then
+    echo -e "$bold$green\nO arquivo $upstream_file foi criado com sucesso!$clear"
+  else
+    echo -e "$bold$red\nErro ao criar arquivo $upstream_file...$clear"
   fi
 }
 
@@ -118,11 +158,19 @@ echo -e "|________________________________________________|$clear"
 echo -e "$green\nSeja bem-vindo ao Assistente de Criação de regras de Proxy!$clear"
 echo -e "$yellow\nAtravés deste wizard você pode criar uma regra de redirecionamento de requisições para seu novo container de app.$clear"
 
-echo -e "$bold\nPor favor, selecione a ação desejada:$clear"
-echo "(1) CRIAR REDIRECIONAMENTO"
-echo "(?) SAIR"
+# Caso o script tenha recebido algum argumento, não exibe o menu principal
+if [[ $# -lt 1 ]]; then
+  echo -e "$bold\nPor favor, selecione a ação desejada:$clear"
+  echo "(1) CRIAR REDIRECIONAMENTO"
+  echo "(?) SAIR"
+fi
 
-read option
+# Antes de receber a opção do usuário, verifica se ela não foi enviada como argumento 
+if [ $# -gt 0 ]; then
+  option=$1
+else
+  read option
+fi
 
 # Acessa o diretório de configurações do Nginx
 cd $APPGTW_PATH/nginx/
@@ -138,7 +186,6 @@ if [[  $option == 1  ]]; then
 
   option1
 else
-
   echo -e "$bold$yellow\nCriação das regras cancelada!\n$clear"
   exit 0
 fi
